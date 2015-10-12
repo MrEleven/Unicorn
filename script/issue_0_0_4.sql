cd04a2423794e35fc027cc69c26fe93c2a9bbe41
July <julycw@gmail.com>
2015-10-12 09:34:51 +0800

    change sql

--- script/issue_0_0_4.sql
+++ script/issue_0_0_4.sql
@@ -1,2 +1,2 @@
 alter table `marker` add column `user_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'user_id' after `title`;
-update marker set user_id = 1;
+update marker set user_id = 1 where user_id = 0 or user_id is null;
