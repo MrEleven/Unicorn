03f2f3a03d7ce4f9a9321935cc0836b5b6cd618d
eleven <iGod_eleven@163.com>
2015-10-25 23:00:45 +0800

    add base interface for ui, good night

--- api/goal.py
+++ api/goal.py
@@ -5,8 +5,50 @@
 # Email : yumi@meishixing.com
 
 from api.base import BaseHandler
+import tornado.web
+import module.goal_ctr as goal_ctrl
 
 class GoalUIHandler(BaseHandler):
     """目标"""
     def get(self):
         return self.render("goal_ui.html", result={})
+
+class AddHandler(BaseHandler):
+    """增加目标"""
+    @tornado.web.authenticated
+    def post(self):
+        user_id = self.get_current_user()
+        name = self.get_argument("name", "")
+        image = self.get_argument("image", "")
+        description = self.get_argument("description", "")
+        goal_id = goal_ctrl.add_goal(user_id, name, image, description)
+        return self.write(goal_id)
+
+class ListHandler(BaseHandler):
+    """获取目标列表"""
+    def get(self):
+        user_id = self.get_argument("user_id", 0)
+        if not user_id:
+            user_id = self.get_current_user()
+        if not user_id:
+            goal_list = []
+        else:
+            user_id = int(user_id)
+            goal_list = goal_ctrl.get_goal_list(user_id)
+        return self.render("goal_list.html", result={"goal_list": goal_list})
+
+class UpdateHandler(BaseHandler):
+    """更新目标"""
+    @tornado.web.authenticated
+    def post(self):
+        goal_id = self.get_argument("goal_id")
+        if not goal_id:
+            raise Exception("goal/update msg: goal_id cant be %s" % goal_id)
+        user_id = self.get_current_user()
+        if not goal_ctrl.check_owner(user_id, goal_id):
+            raise Exception("user_id: %s want to update goal(id: %s) raise no permission Exception")
+        name = self.get_argument("name")
+        image = self.get_argument("image")
+        description = self.get_argument("description")
+        rst = goal_ctrl.update_goal(goal_id, name, image, description)
+        return self.write(rst)
---
+++ module/goal_ctrl.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+# Date  : 2015-10-25
+# Author: Master Yumi
+# Email : yumi@meishixing.com
+
+from config import db
+from datetime import datetime
+
+def add_goal(user_id, name, image, description):
+    """增加目标"""
+    create_time = datetime.now()
+    sql = "insert into goal (user_id, name, image, create_time, description) values (%s, %s, %s, %s, %s);"
+    return db.execute(sql, user_id, name, image, create_time, description)
+
+def get_goal_list(user_id):
+    """获取目标列表"""
+    sql = "select id, name, image, description from goal where user_id = %s;"
+    return db.query(sql, user_id)
+
+def delete_goal(goal_id):
+    """删除目标"""
+    sql = "delete from goal where id = %s;"
+    return db.execute(sql, goal_id)
+
+def update_goal(goal_id, name="", image="", description=""):
+    """更新目标"""
+    update_key_list = []
+    update_value_list = []
+    if name:
+        update_key_list.append("name")
+        update_value_list.append(name)
+    if image:
+        update_key_list.append("image")
+        update_value_list.append(image)
+    if description:
+        update_key_list.append("description")
+        update_value_list.append(description)
+    if not len(update_key_list):
+        return
+    update_cond_tmp = [key + " = %s" for key in update_key_list]
+    update_prefix = ", ".join(update_cond_tmp)
+    sql = "update goal set " + update_prefix + " where id = %s;"
+    return db.execute(sql, **(update_value_list.append(goal_id)))
+
+def check_owner(user_id, goal_id):
+    """验证目标Id和用户Id是否相匹配"""
+    sql = "select goal_id from goal where id = %s and user_id = %s;"
+    return db.query(sql, goal_id, user_id)
+
