b7d8df5f77e5707ca10b1d97e450a850e615ddd4
huiyao <huiyao@wacai.com>
2016-03-03 21:39:12 +0800

    change get random method

--- module/bg_ctrl.py
+++ module/bg_ctrl.py
@@ -6,6 +6,7 @@
 
 from config import db
 from datetime import datetime
+import random
 
 def add_bg(url, image_type=1):
     """增加背景图片"""
@@ -21,5 +22,8 @@ def get_bg_list(image_type=1):
 
 def get_random_bg(image_type=1):
     """获取一张随机背景图片"""
-    sql = "SELECT * FROM bg WHERE status =1 and type = %s and id >= (SELECT floor(RAND() * (SELECT MAX(id) FROM bg where type = %s and status = 1))) ORDER BY id LIMIT 1;"
-    return db.query(sql, image_type, image_type)[0]["url"]
+    sql = "select count(*) as count from bg where status = 1 and type = %s;"
+    count = db.query(sql, image_type)[0]["count"]
+    start = random.randint(0, count-1)
+    sql = "SELECT * FROM bg WHERE status =1 and type = %s LIMIT %s,1;"
+    return db.query(sql, image_type, start)[0]["url"]
